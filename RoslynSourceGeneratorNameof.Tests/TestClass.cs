3037f95ffae994403d04f7f495a371f80a6b9a31
Denis Proshutinskii <jl0pd@outlook.com>
2021-03-20 10:15:28 +0700

    Create code with tests

---
+++ RoslynSourceGeneratorNameof.Tests/TestClass.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace RoslynSourceGeneratorNameof.Tests
+{
+    public class TestClass
+    {
+
+        private const string Sample = @"
+using System.Diagnostics.CodeAnalysis;
+public partial class MyClass
+{
+    [MemberNotNull(nameof(GeneratedProp))]
+    void Foo()
+    {}
+
+    public static void Main() {}
+}";
+
+        [Fact]
+        public void CodeCompiles()
+        {
+            Compilation input = CreateCompilation(Sample);
+            var generator = new MyBuggyGenerator();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+            driver = driver.RunGeneratorsAndUpdateCompilation(input, out var outputCompilation, out var diagnostics);
+
+            Debug.Assert(diagnostics.IsEmpty); // faile here: warning CS8785: Generator 'MyClass' failed to generate source. It will not contribute to the output and compilation errors may occur as a result. Exception was of type 'Exception' with message 'here's bug!'
+            Debug.Assert(outputCompilation.GetDiagnostics().IsEmpty);
+            Debug.Assert(outputCompilation.SyntaxTrees.Count() == 2);
+        }
+
+        private static Compilation CreateCompilation(string source)
+            => CSharpCompilation
+                .Create(
+                    "compilation",
+                    new[] { CSharpSyntaxTree.ParseText(source) },
+                    new[]
+                    {
+                        GetReference<Binder>(),
+                        GetReference<MemberNotNullAttribute>(),
+                    },
+                    new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+        private static PortableExecutableReference GetReference<T>()
+        {
+            return MetadataReference.CreateFromFile(typeof(T).GetTypeInfo().Assembly.Location);
+        }
+    }
+
+
+}
---
+++ RoslynSourceGeneratorNameof/MyBuggyGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynSourceGeneratorNameof
+{
+    [Generator]
+    public class MyBuggyGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForPostInitialization(postCtx =>
+            {
+            });
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.Compilation is not CSharpCompilation compilation || 
+                context.SyntaxContextReceiver is not SyntaxReceiver receiver)
+            {
+                return;
+            }
+
+            var notNull = compilation.GetTypeByMetadataName("System.Diagnostics.CodeAnalysis.MemberNotNullAttribute");
+
+            foreach (var targetClass in receiver.Types)
+            {
+                string className = targetClass.Name;
+                string access = targetClass.DeclaredAccessibility.ToString().ToLower();
+
+                IMethodSymbol[] methods = targetClass.GetMembers().OfType<IMethodSymbol>().ToArray();
+
+                foreach (IMethodSymbol method in methods)
+                {
+                    if (method.GetAttributes().FirstOrDefault(ad => SymbolEqualityComparer.Default.Equals(ad.AttributeClass, notNull)) is AttributeData ad)
+                    {
+                        ImmutableArray<TypedConstant> args =  ad.ConstructorArguments; // actual {System.Diagnostics.CodeAnalysis.MemberNotNullAttribute("")}
+                        if (args[0].Value is "")                                       // should be {System.Diagnostics.CodeAnalysis.MemberNotNullAttribute("GeneratedProp")}
+                        {
+                            throw new Exception("here's bug!");
+                        }
+                    }
+                }
+
+                context.AddSource(className + "_generated.cs", @$"
+                partial class {className}
+                {{
+                    public int GeneratedProp {{ get; set; }}
+                }}");
+            }
+        }
+    }
+
+    internal class SyntaxReceiver : ISyntaxContextReceiver
+    {
+        public List<INamedTypeSymbol> Types { get; } = new();
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax cds)
+            {
+                ISymbol? symbol = context.SemanticModel.GetDeclaredSymbol(cds);
+                if (symbol is INamedTypeSymbol typeSymbol)
+                {
+                    Types.Add(typeSymbol);
+                }
+            }
+        }
+    }
+}
